ce6458b5ee46bfab95d2f6442af8534e4e533962
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-12 16:12:59 +0700

    “menambahkan class KodeProduk”

--- Program.cs
+++ Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            
+            KodeProduk produk = new KodeProduk();
+            Console.Write("Masukkan nama produk: ");
+            string elek = Console.ReadLine();
+            string kodeProd = produk.getKodeProduk(elek);
+
         }
     }
 }
